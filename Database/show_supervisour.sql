38723ba6f8e3f2a1dbf920fbebd9069f98e60fbb
Umesha Jayakody <157822757+UmeshaJayakody@users.noreply.github.com>
2024-10-08 11:59:41 +0000

    delete all

--- Database/show_supervisour.sql
+++
@@ -1,9 +0,0 @@
-delimiter //
-create procedure show_supervisors(
-)
-begin
-	select distinct supervisor.supervisor_id ,employee.first_name,employee.last_name 
-    from supervisor 
-    left join employee on supervisor.supervisor_id=employee.employee_id;
-end //
-delimiter ;
